fbe0873944d8b3a6d7ebf733386faed495fc07ea
Dhaval Koradiya <dhavalk447@gmail.com>
2026-05-04 11:54:44 -0400

    Refactor JwtService for improved readability

--- backend/src/main/java/com/group1/banking/security/JwtService.java
+++ backend/src/main/java/com/group1/banking/security/JwtService.java
@@ -6,7 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service; 
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
